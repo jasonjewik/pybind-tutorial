388e2648a4cf5fe4c0874002d5eb4797a78b6a5f
Jason Jewik <jason.jewik@gmail.comu>
2022-06-08 00:26:44 -0700

    Restructure

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from example import add
+import example
+from example import add, subtract
 
 def main():
+    print(example.__doc__)
+    print('=====')
+
     print(add.__doc__)
     print(f'Default: {add()}')
     print(f'3 + 4 = {add(3, 4)}')
     print(f'6 + 5 = {add(j=5, i=6)}')
+    print('=====')
+
+    print(subtract.__doc__)
+    print(f'Default: {subtract()}')
+    print(f'10 - 4 = {subtract(10, 4)}')
+    print(f'5 - 6 = {subtract(j=6, i=5)}')
 
 if __name__ == '__main__':
     main()
--- src/example.cpp
+++ src/example.cpp
@@ -6,6 +6,10 @@ int add(int i, int j) {
     return i + j;
 }
 
+int subtract(int i, int j) {
+    return i - j;
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
@@ -13,4 +17,5 @@ PYBIND11_MODULE(example, m) {
     // Otherwise, arguments have to be specified like py::arg("i")
     using namespace py::literals;
     m.def("add", &add, "A function that adds two numbers", "i"_a=1, "j"_a=2);
+    m.def("subtract", &subtract, "A function that subtracts two numbers", "i"_a=2, "j"_a=1);
 }
